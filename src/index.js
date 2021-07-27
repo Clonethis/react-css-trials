e623156d7dfb046206f774aff550c4c55c63d24e
unknown <jirka.brtnik@post.cz>
2021-07-27 11:11:46 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styling/main.css";
+import logo from "./styling/images/black_logo_better.jpeg";
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <div className="logo-box">
+          {/* logo on top left of the page */}
+          <img alt="Logo" src={logo} className="logo" />
+        </div>
+        {/* centering whole content */}
+        <div className="center-text-box">
+          {/* Main heading*/}
+          <h1 className="heading-primary">
+            {/* span used to style heading differently */}
+            <span className="heading-primary-main">Snakkuj</span>
+            <span className="heading-primary-secondary">Zdrave a s chuti</span>
+          </h1>
+          <a href="#" className="btn btn-main-white btn-animated">
+            Welcome
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
