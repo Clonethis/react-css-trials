76d0aa51104ec7bba96d917dc1d0c8d89117dd29
unknown <jirka.brtnik@post.cz>
2021-07-28 19:55:50 +0200

    Css updated with sass, run npm run compile:sass to live update changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styling/main.css";
+import "./styling/css/style.css";
 import logo from "./styling/images/black_logo_better.jpeg";
 export default class App extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="logo-box">
+        <div className="header__logo-box">
           {/* logo on top left of the page */}
-          <img alt="Logo" src={logo} className="logo" />
+          <img alt="Logo" src={logo} className="header__logo" />
         </div>
         {/* centering whole content */}
-        <div className="center-text-box">
+        <div className="header__text-box">
           {/* Main heading*/}
           <h1 className="heading-primary">
             {/* span used to style heading differently */}
-            <span className="heading-primary-main">Snakkuj</span>
-            <span className="heading-primary-secondary">Zdrave a s chuti</span>
+            <span className="heading-primary--main">Snakkuj</span>
+            <span className="heading-primary--secondary">Zdrave a s chuti</span>
           </h1>
-          <a href="#" className="btn btn-main-white btn-animated">
+          <a href="#" className="btn btn--main-white btn--animated">
             Welcome
           </a>
         </div>
